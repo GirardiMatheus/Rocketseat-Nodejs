a409bb6ecdfbfb42eba89d5c9ead4bacfd3a0764
Girardi <girardimatheus27@gmail.com>
2024-08-06 16:01:55 -0300

    feat: Implementing Design paterns and tests

--- 03-Implementando-o-SOLID/src/use-cases/register.ts
+++ 03-Implementando-o-SOLID/src/use-cases/register.ts
@@ -1,6 +1,7 @@
 import type { UsersRepository } from "@/repositories/users-repository"
 import { hash } from "bcryptjs"
 import { UserAlreadyExistsError } from "./errors/user-already-exist-error"
+import type { User } from "@prisma/client"
 
 interface RegisterUseCaseRquest {
   name: string
@@ -8,10 +9,14 @@ interface RegisterUseCaseRquest {
   password: string
 }
 
+interface RegisterUseCaseResponse{
+  user: User
+}
+
 export class RegisterUseCase {
   constructor(private usersRepository: UsersRepository) {}
 
-  async execute({ name, email, password,}: RegisterUseCaseRquest) {
+  async execute({ name, email, password,}: RegisterUseCaseRquest): Promise<RegisterUseCaseResponse> {
     const password_hash = await hash(password, 6)
   
     const userWithSameEmail = await this.usersRepository.findByEmail(email)
@@ -20,10 +25,14 @@ export class RegisterUseCase {
       throw new UserAlreadyExistsError()
     }
     
-    await this.usersRepository.create({
+    const user = await this.usersRepository.create({
       name,
       email,
       password_hash,
     })
+
+    return {
+      user,
+    }
   }
 }
